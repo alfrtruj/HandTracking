bfbb3207f03c0a1ef95c7936d57bac1c7c881bc2
alfrtruj <alfrtruj@protonmail.com>
2022-02-08 22:53:34 +0100

    add volume control with pycaw

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,13 +3,17 @@ import time
 from cv2 import FONT_HERSHEY_COMPLEX
 import numpy as np
 import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER  # reference to pycaw created by AndreMiras/pycaw
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 ########################
 wCam, hCam = 640, 480
 #######################
 
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
@@ -17,12 +21,55 @@ pTime = 0
 detector = htm.handDetector(detectionCon=0.7)
 
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol = 0
+volBar = 400
+volPer = 0
+
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     if len(lmList) != 0:
-        print(lmList[2])
+        #print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+
+        cv2.circle(img, (x1, y1), 12, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 12, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255,0,255), 3)
+        cv2.circle(img, (cx, cy), 12, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        #print(length)
+
+        # Hand range 50 - 280 (depends of the hand distance from the camera)
+        # Volumen range -65.25 - 0.0
+
+        vol = np.interp(length, [50, 280], [minVol, maxVol]) # function mapping two range of numbers
+        volBar = np.interp(length, [50, 280], [400, 150])
+        volPer = np.interp(length, [50, 280], [0, 100])
+        #print(int(length), vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 12, (0, 250, 0), cv2.FILLED)
+
+    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+    cv2.putText(img, f' {int(volPer)}%',(30, 450), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
